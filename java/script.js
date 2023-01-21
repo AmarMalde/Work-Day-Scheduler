fe17eae657b6b5d7c97112f59d3aa47f6e0ffd39
[Amar Malde] <105677445+AmarMalde@users.noreply.github.com>
2023-01-21 22:10:59 +0000

    added code in js file to update classes in HTMl file for style purposes. Added comments to describe code

--- java/script.js
+++ java/script.js
@@ -4,3 +4,41 @@ var now = moment().format("dddd, Do MMMM");
 
 //updates HTML elemtent with variable "now"
 $('#currentDay').text(now);
+
+//stores the current hour as a number (1am = 1, 8pm = 20 etc.)
+var hour = moment().format('H');
+
+//convers the hour to an int value
+hour = hour*1
+
+//below function changes the class of the <textareas> in the HTML
+//this is so the current, future and past times can have specific formatting as defined the thier class and css
+function setTense() {
+
+    //iterates through each html element with a class of row
+    $('.row').each(function(i, obj) {
+
+        //if the id of the current element is less than the hour i.e. in the past
+        //therefore change the class of the textarea element to "past"
+        //this is using the initial class of the textarea whihc is always "future"
+
+        if ($(obj).attr('id') < hour) {
+            $(this).children(".future" ).removeClass().addClass('past');
+        } 
+        
+        //if the id of the current element is equal to the hour i.e. the present hour
+        //therefore change the class of the textarea element to "present"
+        
+        else if ($(obj).attr('id') == hour) {
+            $(this).children(".future" ).removeClass().addClass('present');
+        } 
+        
+        //else current element must be in the future
+        //therefore change the class of the textarea element to "future"
+
+        else {
+            $(this).children(".future" ).removeClass().addClass('future');
+        }
+
+    });
+}
